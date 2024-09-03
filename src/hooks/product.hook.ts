396a48123c2f00b36971b086e67c0258c292fea0
kitchanismo <cristian.betsayda@gmail.com>
2024-09-03 20:00:24 +0800

    implements createSlice in redux

--- src/hooks/product.hook.ts
+++ src/hooks/product.hook.ts
@@ -25,7 +25,7 @@ import firebaseDb from '~/utils/firebase'
 import { mapFirebaseDataToProduct } from '~/utils'
 import { useEffect } from 'react'
 import { AppDispatch, RootState } from '~/stores'
-import { setProduct } from '~/stores/product.slice'
+import { actions } from '~/stores/product.slice'
 
 export const useProduct = () => {
   // const productState = useSelector(
@@ -39,60 +39,62 @@ export const useProduct = () => {
 
   useEffect(() => {
     onGetProducts()
-    console.log({ x: productState })
   }, [productState?.selectedTab])
 
   const onGetProducts = () => {
     getProducts().then((products) => {
-      dispatch(setProduct(products))
+      dispatch(actions.setProduct(products))
     })
   }
 
   const onAddProduct = async (product: Product) => {
     await addProduct({ ...product })
+    dispatch(actions.addProduct(product))
   }
 
   const onAddVariant = async (productId: string, variant: Variant) => {
     await addVariant(productId, variant)
+    dispatch(actions.addVariant({ variant, productId }))
   }
 
   const onDeleteProduct = async (productId: string) => {
     await deleteProduct(productId)
-    dispatch(deleteProductAction(productId))
+    dispatch(actions.deleteProduct(productId))
   }
 
   const onDeleteVariant = async (productId: string, variantId: string) => {
     await deleteVariant(productId, variantId)
+    dispatch(actions.deleteVariant({ productId, variantId }))
   }
 
   const onOpenProductForm = (open: boolean) => {
-    dispatch(setOpenProductForm(open))
+    dispatch(actions.setOpenProductForm(open))
   }
 
   const onOpenVariantForm = (open: boolean) => {
-    dispatch(setOpenVariantForm(open))
+    dispatch(actions.setOpenVariantForm(open))
   }
 
   const onSelectedProduct = (product: Product | null) => {
-    dispatch(setSelectedProduct(product))
-    //dispatch(setOpenProductForm(true))
+    dispatch(actions.setSelectedProduct(product))
   }
 
   const onEditProduct = async (product: Product) => {
     await editProduct(product)
+    dispatch(actions.editProduct(product))
   }
 
   const onEditVariant = async (productId: string, variant: Variant) => {
     await editVariant(productId, variant)
+    dispatch(actions.editVariant({ productId, variant }))
   }
 
   const onSelectedVariant = (variant: Variant | null) => {
-    dispatch(setSelectedVariant(variant))
-    dispatch(setOpenVariantForm(true))
+    dispatch(actions.setSelectedVariant(variant))
   }
 
   const onSelectedTab = (tab: string) => {
-    dispatch(setSelectedTab(tab))
+    dispatch(actions.setSelectedTab(tab))
   }
 
   return {
